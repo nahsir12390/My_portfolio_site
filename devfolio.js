8e158f3a651f5bff6bd2dc254413618c1eaf7228
nahsir12390 <157424134+nahsir12390@users.noreply.github.com>
2024-11-09 03:10:10 -0800

    Update devfolio.js

--- devfolio.js
+++ devfolio.js
@@ -1,32 +1,62 @@
-let leftNav =document.getElementById("leftNav")
-let yNav=document.querySelector(".yNav")
-let backToTop =document.querySelector(".backToTop")
-let writeUpDiv =document.querySelectorAll(" .writeUpDiv ")
-let service =document.getElementById("service")  
-let section2 =document.getElementById("section2")
+let vaticalNavContainer =document.getElementById("vaticalNavContainer")
+const openNav = document.querySelector(".open-nav")
+const closeNav = document.querySelector(".close-nav")
+const horizontalNav =document.querySelector(".horizontal-nav")
+const openNavIcon = document.querySelector(".fa-bars")
+let closeNavIcon = document.querySelector(".fa-xmark")
+let darkMode =document.getElementById("darkMode")
+let themeIconContainer=document.querySelector(".theme-icon-container")
+const backToTop = document.getElementById("back-to-top")
 
-function showNav(){
-    yNav.style.display="flex"
-    window.scrollY
-}
+console.log(backToTop)
 
-function closeNav(){
-    yNav.style.display="none"
-}
-
- 
+document.body.style.transition="2s "
 
 
-   window.addEventListener("scroll", ()=>{
-    if(window.scrollY>150){
-        backToTop.classList.add("backToTopShow")
 
-    }else{
-     backToTop.classList.remove("backToTopShow")
-    }
-   })
-
-backToTop.addEventListener("click", ()=>{
-    window.scrollTo({top:0, behavior:"smooth"});
+vaticalNavContainer.style.display="none"
+openNav.addEventListener("click", (event)=>{
+    vaticalNavContainer.style.display="flex"
+    horizontalNav.style.display="none"
+    horizontalNav.style.transition="2s"
 })
 
+closeNav.addEventListener("click", (event)=>{
+    vaticalNavContainer.style.display="none"
+    horizontalNav.style.display="flex"
+})
+
+themeIconContainer.addEventListener("click",(e)=>{
+
+        darkMode.classList.toggle("bi-lightbulb")
+        darkMode.classList.toggle("light-icon")
+if(darkMode.classList.toggle!=="bi-lightbulb"){
+document.body.classList.toggle("dark-theme")
+openNav.classList.toggle("light-icon")
+closeNav.classList.toggle("light-icon")
+
+}
+    
+})
+
+
+function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
+
+
+  backToTop.addEventListener('click', scrollToTop);
+
+
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 500) { 
+      backToTop.style.display = 'block';
+    } else {
+      backToTop.style.display = 'none';
+    }
+  });
+ // ScrollReveal().reveal('.section2');
+  
+      
+
+      
